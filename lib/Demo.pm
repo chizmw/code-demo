03ab66028249e5b8c950547d0ff0dd81904a6e65
Chisel <chisel@chizography.net>
2011-12-05 20:49:17 +0000

    Remove default POD

--- lib/Demo.pm
+++ lib/Demo.pm
@@ -4,18 +4,6 @@ use namespace::autoclean;
 
 use Catalyst::Runtime 5.80;
 
-# Set flags and add plugins for the application.
-#
-# Note that ORDERING IS IMPORTANT here as plugins are initialized in order,
-# therefore you almost certainly want to keep ConfigLoader at the head of the
-# list if you're using it.
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
 use Catalyst qw/
     -Debug
     ConfigLoader
@@ -26,15 +14,6 @@ extends 'Catalyst';
 
 our $VERSION = '0.01';
 
-# Configure the application.
-#
-# Note that settings in demo.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
-
 __PACKAGE__->config(
     name => 'Demo',
     # Disable deprecated behavior needed by old applications
@@ -43,33 +22,5 @@ __PACKAGE__->config(
 
 # Start the application
 __PACKAGE__->setup();
-
-
-=head1 NAME
-
-Demo - Catalyst based application
-
-=head1 SYNOPSIS
-
-    script/demo_server.pl
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 SEE ALSO
-
-L<Demo::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-Chisel Wright,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
